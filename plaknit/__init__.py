1d858d0ee44ec01c033eaa6f0514285b0a68472f
dzfinch <dryver2206@gmail.com>
2025-11-09 17:45:22 -0500

    modified:   README.md 	modified:   docs/index.md 	modified:   docs/usage.md 	modified:   plaknit/__init__.py 	new file:   plaknit/classify.py 	modified:   requirements.txt

--- plaknit/__init__.py
+++ plaknit/__init__.py
@@ -5,6 +5,7 @@ from .analysis import (
     normalized_difference_from_files,
     normalized_difference_from_raster,
 )
+from .classify import predict_rf, train_rf
 
 __author__ = """Dryver Finch"""
 __email__ = "dryver2206@gmail.com"
@@ -14,4 +15,6 @@ __all__ = [
     "normalized_difference",
     "normalized_difference_from_raster",
     "normalized_difference_from_files",
+    "train_rf",
+    "predict_rf",
 ]
