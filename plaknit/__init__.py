f82a6b05685235fb80864e70c77537169af44c29
dzfinch <dryver2206@gmail.com>
2026-01-23 10:34:27 -0500

    modified:   plaknit/__init__.py

--- plaknit/__init__.py
+++ plaknit/__init__.py
@@ -11,7 +11,7 @@ from .planner import plan_monthly_composites, write_plan
 
 __author__ = """Dryver Finch"""
 __email__ = "dryver2206@gmail.com"
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 __all__ = [
     "normalized_difference",
