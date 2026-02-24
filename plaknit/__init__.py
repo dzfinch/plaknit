fee0ae33a8c7f955e6d2ad8d878c218e936f7b39
dzfinch <dryver2206@gmail.com>
2026-02-24 15:39:42 -0500

    deleted:    mcp.json 	modified:   plaknit/__init__.py 	modified:   pyproject.toml 	deleted:    tx202103.json

--- plaknit/__init__.py
+++ plaknit/__init__.py
@@ -6,7 +6,7 @@ from .planner import plan_monthly_composites, write_plan
 
 __author__ = """Dryver Finch"""
 __email__ = "dryver2206@gmail.com"
-__version__ = "0.2.9"
+__version__ = "0.3.0"
 
 __all__ = [
     "train_rf",
